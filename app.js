667ea1ac33d5339bdae7cf731d2667c74ad0198a
Nikita Chebykin <nikita@chebyk.in>
2013-11-12 18:13:49 +0400

    remove console log after starting app

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.locals.message = {};
 app.use('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
 });
 
 module.exports = app;
